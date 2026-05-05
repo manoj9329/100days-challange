e92949f6f41904592eaa997d04b67867a02fa097
manoj9329 <manojsmanu9329@gmail.com>
2026-05-05 12:14:34 +0530

    added progarm

---
+++ roman_to_integer.java
@@ -0,0 +1,39 @@
+import java.util.HashMap;
+public class roman_to_integer {
+    public static int romanToint(String s){
+
+        HashMap<Character, Integer> m = new HashMap<>();
+        m.put('I', 1);
+        m.put('V', 5);
+        m.put('X', 10);
+        m.put('L', 50);
+        m.put('C', 100);
+        m.put('D', 500);
+        m.put('M', 1000);
+
+        int result = m.get(s.charAt(s.length()-1));
+        for(int i = s.length()-2; i>=0; i--){
+            if (m.get(s.charAt(i)) < m.get(s.charAt(i + 1))) {
+                
+                result  = result - m.get(s.charAt(i));
+            
+            } else {
+                result = result + m.get(s.charAt(i));
+            }
+        }
+
+        return result;
+
+
+    }
+    public static void main(String[] args) {
+        
+
+        String roman = "MCMIV";
+        int value = romanToint(roman);
+
+        System.out.println("Roman: " + roman);
+        System.out.println("Integer: " + value);
+    
+    }
+}
